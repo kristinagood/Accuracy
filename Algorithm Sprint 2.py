a244654df99877ffb2f1280f0296483a694249a3
kristinagood <79094550+kristinagood@users.noreply.github.com>
2021-04-16 16:15:31 -0400

    Add files via upload
    sprint 2

---
+++ Algorithm Sprint 2.py
@@ -0,0 +1,13 @@
+Start
+Set to loop 5 times
+Set speed to 50
+    Spin for 360 degrees for 10 seconds
+    Spin -360 degrees for 10 seconds
+stop
+Speak "I am the winner" and wait
+Set fade from green to pink for 1 second
+Set fade from pink to red for 1 second
+Set fade from red to yellow for 1 second
+Set fade from yellow to blue for 1 second
+Set fade from blue to orange for 1 second
+Exit program
